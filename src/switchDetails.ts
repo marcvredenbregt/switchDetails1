26eee20fdaa61310bb12d9cd9209ef549e9357d9
marcvredenbregt <marcvredenbregt@rysk-it.net>
2024-02-05 21:32:47 +0100

    switchDetails.ts

--- src/switchDetails.ts
+++ src/switchDetails.ts
@@ -1,5 +1,5 @@
-const message = document.querySelector('.message')
-const button = document.querySelector('button')
+const message = document.querySelector('.message') as HTMLInputElement
+const button = document.querySelector('button') as HTMLInputElement
 
 button.addEventListener('click', () => fetchFile())
 function fetchFile() {
@@ -8,7 +8,7 @@ function fetchFile() {
         method: "POST"
     })
     .then(response => response.text())
-    .then((data) => {
+    .then((data: string) => {
 
         // Convert the switchDetails-array to a JSON-formatted string
         // var jsonString = JSON.stringify(switchData, null, 2)
@@ -21,9 +21,9 @@ function fetchFile() {
     .catch(error => console.error("Error processing data: ", error))
 }
 
-function downloadJson(jsonString) {
+function downloadJson(jsonString: string) {
 
-    const downloadJson = document.querySelector('.download')
+    const downloadJson = document.querySelector('.download') as HTMLInputElement
 
     // Create a Blob with the JSON string
     var blob = new Blob([jsonString], { type: "application/json" })
